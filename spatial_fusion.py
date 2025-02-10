3b47cfe9c7aef2b0695b557ad8f54118df6ee2c3
aeiyansiddiqi <siddiqiaeiyan@outlook.com>
2025-02-09 19:46:24 -0500

    more changes

--- spatial_fusion.py
+++ spatial_fusion.py
@@ -3,3 +3,22 @@
     # using Gaussian/Lapacian pyramids
 # Must produce and save spatial_hybrid.jpg
 #JPG
+
+# i gotta find away to makea gausian pryramid
+# Gausia pyramid is smoothing the image
+# but also down sampling it
+# I know pyrDown helps with down sampling
+# But I also need to smooth it out
+# Found a function that smooths
+    # GaussianBlur, perfect
+
+import cv2
+
+def gaussianPyramid(imageFileName, numberofLayers):
+
+    imageGausPyrmaid = cv2.imread(imageFileName)
+    
+
+    for numberofLayer in range(numberofLayers):
+        imageGausPyrmaid = cv2.GaussianBlur(imageGausPyrmaid,(7,7),0)
+        imageGausPyrmaid = cv2.pyrDown(imageGausPyrmaid)
